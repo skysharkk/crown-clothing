1f7a39def61c9d867f5ae21923fa3288c021e376
skysharkk <sky.light.blr@gmail.com>
2020-11-25 21:34:59 +0300

    init: add firebase config

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,12 @@
+const config = {
+	apiKey: 'AIzaSyAn71j77gpihHPmEQ5BN8H8e7n4z216uw0',
+	authDomain: 'crown-clothing-db-portfolio.firebaseapp.com',
+	databaseURL: 'https://crown-clothing-db-portfolio.firebaseio.com',
+	projectId: 'crown-clothing-db-portfolio',
+	storageBucket: 'crown-clothing-db-portfolio.appspot.com',
+	messagingSenderId: '365351366008',
+	appId: '1:365351366008:web:957ffcf9ddacfeb3e92c8d',
+	measurementId: 'G-WRREDSGVHX',
+};
+
+export default config;
