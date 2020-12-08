a32eef61f2215559619ef55832ef2af7e413a850
skysharkk <sky.light.blr@gmail.com>
2020-12-08 22:52:05 +0300

    feat: implement directory component

---
+++ src/components/directory/directory.component.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { selectDirectory } from '../../redux/directory/directory.selectors';
+
+import { IDirectoryState } from '../../redux/directory/directory.types';
+import { AppState } from '../../redux/state.types';
+
+import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.styles.scss';
+
+interface IStateProps {
+	directory: IDirectoryState;
+}
+
+type Props = IStateProps;
+
+const Directory: React.FC<Props> = ({
+	directory: { directoryData },
+}: Props) => {
+	return (
+		<div className='directory-container'>
+			{directoryData
+				? directoryData.map(({ id, imageUrl, linkUrl, size, title }) => (
+						<MenuItem
+							key={id}
+							id={id}
+							imageUrl={imageUrl}
+							linkUrl={linkUrl}
+							size={size}
+							title={title}
+						/>
+				  ))
+				: null}
+		</div>
+	);
+};
+
+const mapStateToProps = (state: AppState) => ({
+	directory: selectDirectory(state),
+});
+
+export default connect(mapStateToProps)(Directory);
---
+++ src/components/directory/directory.container.tsx
@@ -0,0 +1,25 @@
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import Directory from './directory.component';
+import WithSpinner from '../with-spinner/with-spinner.component';
+import { selectIsDirectoryLoading } from '../../redux/directory/directory.selectors';
+import { AppState } from '../../redux/state.types';
+
+type DirectoryLoadingType = ReturnType<typeof selectIsDirectoryLoading>;
+
+const mapStateToProps = createStructuredSelector<
+	AppState,
+	{ isLoading: DirectoryLoadingType }
+>({
+	isLoading: selectIsDirectoryLoading,
+});
+
+const DirectoryContainer = compose(
+	connect(mapStateToProps),
+	WithSpinner
+)(Directory);
+
+// eslint-disable-next-line prettier/prettier
+export default DirectoryContainer as React.ComponentType;
