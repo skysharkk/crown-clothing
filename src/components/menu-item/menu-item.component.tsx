a2859d8273ded817858598a86b6f81b6eee6bb5a
skysharkk <sky.light.blr@gmail.com>
2020-12-08 22:56:38 +0300

    feat: implement menu item component

---
+++ src/components/menu-item/menu-item.component.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './menu-item.styles.scss';
+import { IDirectoryItemData } from '../../redux/directory/directory.types';
+
+type Props = IDirectoryItemData;
+
+const MenuItem: React.FC<Props> = ({
+	imageUrl,
+	linkUrl,
+	size,
+	title,
+}: Props) => {
+	return (
+		<div className={`menu-item menu-item_${size}`}>
+			<div
+				className='menu-item__background'
+				style={{ backgroundImage: `url(${imageUrl})` }}
+			/>
+
+			<Link className='menu-item__link' to={linkUrl}>
+				<div className='menu-item__title-container'>
+					<h2 className='menu-item__title'>{title.toUpperCase()}</h2>
+					<span className='menu-item__subtitle'>SHOP NOW</span>
+				</div>
+			</Link>
+		</div>
+	);
+};
+
+export default MenuItem;
