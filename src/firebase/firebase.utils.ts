bcd4f1a2be55554e3760c99387565b3b0d56997f
skysharkk <sky.light.blr@gmail.com>
2020-12-08 22:49:36 +0300

    feat: implement get collection data function

---
+++ src/firebase/firebase.utils.ts
@@ -0,0 +1,28 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import config from "./firebase.config";
+
+firebase.initializeApp(config);
+
+export const dataBase = firebase.firestore();
+
+export async function getCollectionDataArray<T>(
+  collectionName: string,
+  order = ""
+): Promise<Array<T> | null> {
+  const collectionRef = dataBase.collection(collectionName);
+  let snapShot;
+  try {
+    snapShot = order
+      ? await collectionRef.orderBy(order).get()
+      : await collectionRef.get();
+  } catch (error) {
+    console.error("Error getting collection", error);
+  }
+  if (snapShot) {
+    // eslint-disable-next-line prettier/prettier
+    return snapShot.docs.map((doc) => doc.data() as T);
+  }
+  return null;
+}
